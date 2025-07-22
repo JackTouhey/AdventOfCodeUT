d61a572b472dab10a30e241ca88b9b294104a814
Jack Touhey <jt112112@gmail.com>
2025-07-22 16:05:29 +1000

    Tested coordinateSet.contains and begun loadRegions method

--- src/main/java/com/example/adventofcode/solutions/DayTwelve2024.java
+++ src/main/java/com/example/adventofcode/solutions/DayTwelve2024.java
@@ -7,8 +7,22 @@ import com.example.adventofcode.utils.DataLoader;
 
 public class DayTwelve2024 {
     private static final char[][] garden = DataLoader.generateCharGrid("DataFiles\\DayTwelveTestData.txt");
+    private static final HashSet<Region> regions = loadRegions();
     public static void main(String[] args) {
         printGarden();
+    }
+    private static HashSet<Region> loadRegions(){
+        HashSet<Region> returnSet = new HashSet<>();
+        HashSet<Coordinate> loadedPlots = new HashSet<>();
+        for(int y = 0; y < garden.length; y++){
+            for(int x = 0; x < garden[y].length; x++){
+                Coordinate currentCoordinate = new Coordinate(x, y);
+                if(!loadedPlots.contains(currentCoordinate)){
+
+                }
+            }
+        }
+        return returnSet;
     }
     private static void printGarden(){
         for(char[] row : garden){
--- src/test/java/com/example/adventofcode/CoordinateTesting.java
+++ src/test/java/com/example/adventofcode/CoordinateTesting.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 import java.util.HashSet;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,21 +12,30 @@ import org.junit.jupiter.api.Test;
 import com.example.adventofcode.utils.Coordinate;
 
 class CoordinateTesting {
-
+    private final HashSet<Coordinate> coordSet = new HashSet<>();;
     @BeforeEach
     void setUp() {
-
-    }
-    
-    @Test
-    @DisplayName("Hashset coordinate test")
-    void testCoordinate() {
         Coordinate c1 = new Coordinate(1, 1);
         Coordinate c2 = new Coordinate(1, 1);
         Coordinate c3 = new Coordinate(1, 2);
 
-        HashSet<Coordinate> coordSet = new HashSet<>();
         Collections.addAll(coordSet, c1, c2, c3);
+    }
+    
+    @Test
+    @DisplayName("Hashset coordinate test")
+    void testHashSetSize() {
         assertEquals(2, coordSet.size());
+    }
+
+    @Test
+    void testHashSetContains(){
+        Coordinate c1 = new Coordinate(1, 1);
+        assertTrue(coordSet.contains(c1));
+    }
+    @Test
+    void testHashSetContainsFailState(){
+        Coordinate c1 = new Coordinate(2, 2);
+        assertTrue(coordSet.contains(c1));
     }
 }

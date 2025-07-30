878e01d3449db5e1553ae5646f6dede7b7441d5b
Jack Touhey <jt112112@gmail.com>
2025-07-30 14:57:53 +1000

    Robots now know grid dimensions

--- src/main/java/com/example/adventofcode/solutions/DayFourteen2024.java
+++ src/main/java/com/example/adventofcode/solutions/DayFourteen2024.java
@@ -6,7 +6,7 @@ import com.example.adventofcode.utils.DataLoader;
 import com.example.adventofcode.utils.Robot;
 
 public class DayFourteen2024 {
-    private static final ArrayList<Robot> robots = DataLoader.loadDayFourteen("DataFiles\\DayFourteenTestData.txt");
+    private static final ArrayList<Robot> robots = DataLoader.loadDayFourteen("DataFiles\\DayFourteenTestData.txt", 7, 11);
     public static void main(String[] args) {
         for(Robot r : robots){
             System.out.println(r.toString());
--- src/main/java/com/example/adventofcode/utils/Robot.java
+++ src/main/java/com/example/adventofcode/utils/Robot.java
@@ -1,11 +1,15 @@
 package com.example.adventofcode.utils;
 
 public class Robot {
+    private final int gridHeight;
+    private final int gridWidth;
     private Coordinate position;
     private final Coordinate velocity;
-    public Robot(Coordinate startingPosition, Coordinate velocity){
+    public Robot(Coordinate startingPosition, Coordinate velocity, int gridHeight, int gridWidth){
         this.position = startingPosition;
         this.velocity = velocity;
+        this.gridHeight = gridHeight;
+        this.gridWidth = gridWidth;
     }
     @Override
     public String toString() {

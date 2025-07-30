90c10ae384795e52c06e2a202e8cc9e1ac997b63
Jack Touhey <jt112112@gmail.com>
2025-07-30 15:31:47 +1000

    Unit tested moving in Y plane

--- src/test/java/com/example/adventofcode/RobotTesting.java
+++ src/test/java/com/example/adventofcode/RobotTesting.java
@@ -31,4 +31,45 @@ public class RobotTesting {
         r.moveOnce();
         assertEquals(3, r.getPosition().getX());
     }
+    @Test
+    void moveLeftWithinBounds(){
+        Coordinate start = new Coordinate(5,0);
+        Coordinate velocity = new Coordinate(-3, 0);
+        Robot r = new Robot(start, velocity, 7, 11);
+        r.moveOnce();
+        assertEquals(2, r.getPosition().getX());
+    }
+    @Test
+    void moveOffTopSideTest(){
+        Coordinate start = new Coordinate(0,0);
+        Coordinate velocity = new Coordinate(0, -3);
+        Robot r = new Robot(start, velocity, 7, 11);
+        r.moveOnce();
+        assertEquals(4, r.getPosition().getY());
+    }
+    @Test
+    void moveOffBottomSideTest(){
+        Coordinate start = new Coordinate(0,7);
+        Coordinate velocity = new Coordinate(0, 3);
+        Robot r = new Robot(start, velocity, 7, 11);
+        r.moveOnce();
+        assertEquals(3, r.getPosition().getY());
+    }
+    @Test
+    void moveUpWithinBounds(){
+        Coordinate start = new Coordinate(0,5);
+        Coordinate velocity = new Coordinate(0, -3);
+        Robot r = new Robot(start, velocity, 7, 11);
+        r.moveOnce();
+        assertEquals(2, r.getPosition().getY());
+    }
+    @Test
+    void moveDownWithinBounds(){
+        Coordinate start = new Coordinate(0,2);
+        Coordinate velocity = new Coordinate(0, 3);
+        Robot r = new Robot(start, velocity, 7, 11);
+        r.moveOnce();
+        assertEquals(5, r.getPosition().getY());
+    }
+    
 }

eea6f4c81f46d07a3a720beb76751599f8a65b9f
Jack Touhey <jt112112@gmail.com>
2025-08-07 14:49:48 +1000

    Added class for commonly used prints to System.Out

---
+++ src/main/java/com/example/adventofcode/utils/SystemOut.java
@@ -0,0 +1,13 @@
+package com.example.adventofcode.utils;
+
+public class SystemOut {
+    public SystemOut(){}
+    private static void printGrid(String[][] grid){
+        for(String[] y : grid){
+            for (String x : y) {
+                System.out.print(x);
+            }
+            System.out.println();
+        }
+    }
+}

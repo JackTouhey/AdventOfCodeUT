eb1bf85c9656358c98c609d23c53e15c8d9a8205
Jack Touhey <jt112112@gmail.com>
2025-08-15 12:10:53 +1000

    Unit tested getMazeStart/End

---
+++ src/test/java/com/example/adventofcode/DaySixteenTesting.java
@@ -0,0 +1,16 @@
+package com.example.adventofcode;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import com.example.adventofcode.solutions.DaySixteen2024;
+import com.example.adventofcode.utils.Coordinate;
+
+public class DaySixteenTesting {
+    private final DaySixteen2024 day16 = new DaySixteen2024("DataFiles\\DaySixteenTestData.txt");
+    @Test
+    void testGetMazeStartAndEnd(){
+        assertEquals(day16.getMazeStart(), new Coordinate(1, 13));
+        assertEquals(day16.getMazeEnd(), new Coordinate(13, 1));
+    }
+}

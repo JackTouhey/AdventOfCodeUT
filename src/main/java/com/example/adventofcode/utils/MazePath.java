a22bdf7ee75076fdd5c4499c67defb62cf9a6b8d
Jack Touhey <jt112112@gmail.com>
2025-08-15 12:12:42 +1000

    Added overrides to make MazePath work in HashSet

--- src/main/java/com/example/adventofcode/utils/MazePath.java
+++ src/main/java/com/example/adventofcode/utils/MazePath.java
@@ -1,6 +1,7 @@
 package com.example.adventofcode.utils;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class MazePath {
     private ArrayList<Character> steps;
@@ -14,4 +15,17 @@ public class MazePath {
     }
     public ArrayList<Character> getSteps(){return this.steps;}
     public void addStep(Character c){steps.add(c);}
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        MazePath mazePath = (MazePath) obj;
+        return Objects.equals(steps, mazePath.steps);
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(steps);
+    }
 }

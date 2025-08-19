68be78b13fbbf677c238580f6be86d65ca2638ad
Jack Touhey <jt112112@gmail.com>
2025-08-19 13:50:15 +1000

    Now loading registers A., B, and C

---
+++ src/main/java/com/example/adventofcode/solutions/DaySeventeen2024.java
@@ -0,0 +1,19 @@
+package com.example.adventofcode.solutions;
+
+import java.util.ArrayList;
+
+import com.example.adventofcode.utils.DataLoader;
+
+public class DaySeventeen2024 {
+    private int registerA;
+    private int registerB;
+    private int registerC;
+    private ArrayList<Integer> program;
+    private int instructionPointer;
+    public DaySeventeen2024(String filePath){
+        this.registerA = DataLoader.daySeventeenRegisterA(filePath);
+        this.registerB = DataLoader.daySeventeenRegisterB(filePath);
+        this.registerC = DataLoader.daySeventeenRegisterC(filePath);
+        System.out.println(registerA + " register b: " + registerB + " registerc: " + registerC);
+    }
+}

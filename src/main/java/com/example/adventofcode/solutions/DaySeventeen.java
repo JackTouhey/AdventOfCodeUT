747389cc00442b7a3a3931c9e19374de54aad4f6
Jack Touhey <jt112112@gmail.com>
2025-08-19 13:37:40 +1000

    Added DaySeventeen and data files

---
+++ src/main/java/com/example/adventofcode/solutions/DaySeventeen.java
@@ -0,0 +1,14 @@
+package com.example.adventofcode.solutions;
+
+import java.util.ArrayList;
+
+public class DaySeventeen {
+    private int registerA;
+    private int registerB;
+    private int registerC;
+    private ArrayList<Integer> program;
+    private int instructionPointer;
+    public DaySeventeen(String filePath){
+
+    }
+}
